b10004b977bb67a2d60dc19ff3c54a288c1e6963
Jack251970 <1160210343@qq.com>
2023-11-25 17:10:37 +0800

    Add clock page.

--- DesktopWidgets3/Services/WidgetManagerService.cs
+++ DesktopWidgets3/Services/WidgetManagerService.cs
@@ -1,6 +1,5 @@
 ﻿using DesktopWidgets3.Contracts.Services;
 using DesktopWidgets3.Models;
-using DesktopWidgets3.ViewModels.WidgetsPages.Clock;
 using DesktopWidgets3.Views.Windows;
 using Microsoft.UI.Xaml.Controls;
 
@@ -8,17 +7,24 @@ namespace DesktopWidgets3.Services;
 
 public class WidgetManagerService : IWidgetManagerService
 {
-    private readonly Dictionary<WidgetType, BlankWindow> WidgetsDict = new() {};
+    private readonly Dictionary<WidgetType, BlankWindow> WidgetsDict = new();
+
+    private readonly List<WidgetType> TimerWidgets = new()
+    {
+        WidgetType.Clock,
+    };
 
     private readonly IActivationService _activationService;
     private readonly IAppSettingsService _appSettingsService;
     private readonly IThemeSelectorService _themeSelectorService;
+    private readonly ITimersService _timersService;
 
-    public WidgetManagerService(IActivationService activationService, IAppSettingsService appSettingsService, IThemeSelectorService themeSelectorService)
+    public WidgetManagerService(IActivationService activationService, IAppSettingsService appSettingsService, IThemeSelectorService themeSelectorService, ITimersService timersService)
     {
         _activationService = activationService;
         _appSettingsService = appSettingsService;
         _themeSelectorService = themeSelectorService;
+        _timersService = timersService;
     }
 
     public void InitializeWidgets()
@@ -27,12 +33,21 @@ public class WidgetManagerService : IWidgetManagerService
         return;
 #else
         var widgetList = _appSettingsService.GetWidgetsList();
+        var enableTimer = false;
         foreach (var widget in widgetList)
         {
             if (widget.IsEnabled)
             {
                 ShowWidget(WidgetItemUtils.ConvertToBaseWidgetItem(widget).Type);
+                if (timerWidgets.Contains(widget.Type))
+                {
+                    enableTimer = true;
+                }
             }
+        }
+        if (enableTimer)
+        {
+            _timersService.StartUpdateTimeTimer();
         }
 #endif
     }
@@ -52,6 +67,10 @@ public class WidgetManagerService : IWidgetManagerService
         {
             widgetWindow.Show();
         }
+        if (TimerWidgets.Contains(widgetType))
+        {
+            _timersService.StartUpdateTimeTimer();
+        }
     }
 
     public void CloseWidget(WidgetType widgetType)
@@ -60,6 +79,10 @@ public class WidgetManagerService : IWidgetManagerService
         {
             widgetWindow.Close();
             WidgetsDict.Remove(widgetType);
+        }
+        if (!WidgetsDict.Keys.Any(TimerWidgets.Contains))
+        {
+            _timersService.StopUpdateTimeTimer();
         }
     }
 
--- DesktopWidgets3/ViewModels/WidgetsPages/Clock/ClockViewModel.cs
+++ DesktopWidgets3/ViewModels/WidgetsPages/Clock/ClockViewModel.cs
@@ -1,15 +1,26 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using DesktopWidgets3.Helpers;
+using DesktopWidgets3.Contracts.Services;
+using Microsoft.UI.Dispatching;
 
 namespace DesktopWidgets3.ViewModels.WidgetsPages.Clock;
 
 public partial class ClockViewModel : ObservableRecipient
 {
     [ObservableProperty]
-    private string _appDisplayName;
+    private string _systemTime = string.Empty;
 
-    public ClockViewModel()
+    private readonly string timingFormat = string.Empty;
+
+    private readonly DispatcherQueue _dispatcherQueue = App.MainWindow!.DispatcherQueue;
+
+    public ClockViewModel(ITimersService timersService)
     {
-        _appDisplayName = "AppDisplayName".GetLocalized();
+        timingFormat = "T";
+        timersService.InitializeUpdateTimeTimer(UpdateTime);
+    }
+
+    private void UpdateTime(object? sender, EventArgs e)
+    {
+        _dispatcherQueue.TryEnqueue(() => SystemTime = DateTime.Now.ToString(timingFormat));
     }
 }
