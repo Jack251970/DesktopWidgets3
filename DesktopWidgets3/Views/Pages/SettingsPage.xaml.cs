cbe453589facba1da4d77b04de669b1198d059d9
Jack251970 <1160210343@qq.com>
2023-12-03 22:38:18 +0800

    整理

--- DesktopWidgets3/Views/Pages/SettingsPage.xaml.cs
+++ DesktopWidgets3/Views/Pages/SettingsPage.xaml.cs
@@ -4,7 +4,6 @@ using DesktopWidgets3.ViewModels.Pages;
 
 namespace DesktopWidgets3.Views.Pages;
 
-// TODO: Set the URL for your privacy policy by updating Settings_PrivacyTermsLink.NavigateUri in Resources.resw.
 public sealed partial class SettingsPage : Page
 {
     public SettingsViewModel ViewModel
