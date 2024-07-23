56553fd50015496e18d2d16bcf58aeab50066f42
Jack251970 <1160210343@qq.com>
2024-07-23 22:08:23 +0800

    优化了托盘资源的管理 优化代码质量

---
+++ DesktopWidgets3/UserControls/TrayMenuControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls;
+
+namespace DesktopWidgets3.UserControls;
+
+#pragma warning disable CA1822 // Mark members as static
+
+public sealed partial class TrayMenuControl : UserControl
+{
+    public TrayMenuControl()
+    {
+        InitializeComponent();
+#if DEBUG
+        TrayIconControl.ToolTipText += " (Debug)";
+#endif
+    }
+
+    [RelayCommand]
+    private void ShowWindow()
+    {
+        App.ShowMainWindow(false);
+        ApplicationLifecycleExtensions.MainWindow_Closing?.Invoke(App.MainWindow, null!);
+    }
+
+    [RelayCommand]
+    private async Task ExitApp()
+    {
+        try
+        {
+            TrayIconControl.Dispose();
+        }
+        catch
+        {
+            
+        }
+        App.CanCloseWindow = true;
+        await WindowsExtensions.CloseWindow(App.MainWindow);
+    }
+}
