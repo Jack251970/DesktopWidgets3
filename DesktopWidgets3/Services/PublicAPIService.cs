6348454336ea8bbc52d01116f8bc1dcfd7888a5c
Jack251970 <1160210343@qq.com>
2024-09-08 18:37:47 +0800

    fix compiling issue

--- DesktopWidgets3/Services/PublicAPIService.cs
+++ DesktopWidgets3/Services/PublicAPIService.cs
@@ -4,7 +4,7 @@ internal class PublicAPIService : IPublicAPIService
 {
     public T LoadSettingJsonStorage<T>() where T : new()
     {
-
+        return new T();
     }
 
     public void SaveSettingJsonStorage<T>() where T : new()
