409674d9388a09431377e91fcc2e3f73619c875b
Jack251970 <1160210343@qq.com>
2024-09-10 20:04:16 +0800

    add core.widget project

---
+++ DesktopWidgets3.Core.Widgets/Usings.cs
@@ -0,0 +1,16 @@
+﻿// WinUIEx
+global using WinUIEx;
+
+// DesktopWidgets3.Core
+global using DesktopWidgets3.Core.Contracts.Services;
+global using DesktopWidgets3.Core.Extensions;
+global using DesktopWidgets3.Core.Helpers;
+global using DesktopWidgets3.Core.Views.Windows;
+
+// DesktopWidgets3.Infrastructure
+global using DesktopWidgets3.Infrastructure.Contracts.Services;
+global using DesktopWidgets3.Infrastructure.Extensions;
+global using DesktopWidgets3.Infrastructure.Helpers.Exception;
+global using DesktopWidgets3.Infrastructure.Helpers.Json;
+global using DesktopWidgets3.Infrastructure.Helpers.Runtime;
+global using DesktopWidgets3.Infrastructure.Services;
