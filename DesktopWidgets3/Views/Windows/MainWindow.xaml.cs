bc26bdb001cf410b33802320252010ee8c79896b
Jack251970 <1160210343@qq.com>
2024-09-24 23:46:50 +0800

    improve code quality

--- DesktopWidgets3/Views/Windows/MainWindow.xaml.cs
+++ DesktopWidgets3/Views/Windows/MainWindow.xaml.cs
@@ -74,9 +74,12 @@ public sealed partial class MainWindow : WindowEx
         var monitorInfo = DisplayMonitor.GetMonitorInfo(this);
         var rectWorkWidth = monitorInfo.RectWork.Width;
         var rectWorkHeight = monitorInfo.RectWork.Height;
-        var windowWidth = AppWindow.Size.Width;
-        var windowHeight = AppWindow.Size.Height;
-        Position = new PointInt32((int)((rectWorkWidth! - windowWidth) / 2), (int)((rectWorkHeight! - windowHeight) / 2));
+        if (rectWorkWidth != null && rectWorkHeight != null)
+        {
+            var windowWidth = AppWindow.Size.Width;
+            var windowHeight = AppWindow.Size.Height;
+            Position = new PointInt32((int)((rectWorkWidth - windowWidth) / 2), (int)((rectWorkHeight - windowHeight) / 2));
+        }
     }
 
     #region Hide & Show & Activate
