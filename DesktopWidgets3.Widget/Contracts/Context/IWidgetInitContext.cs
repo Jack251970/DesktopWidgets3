336e3512368d5439d096b019b31958fe4f18b5ab
Jack251970 <1160210343@qq.com>
2024-10-21 11:23:15 +0800

    improve code quality

--- DesktopWidgets3.Widget/Contracts/Context/IWidgetInitContext.cs
+++ DesktopWidgets3.Widget/Contracts/Context/IWidgetInitContext.cs
@@ -1,16 +1,37 @@
 ﻿namespace DesktopWidgets3.Widget;
 
+/// <summary>
+/// Context to provide information and functions for one widget group.
+/// </summary>
 public interface IWidgetInitContext
 {
+    /// <summary>
+    /// Widget group metadata from widget.json file.
+    /// </summary>
     WidgetGroupMetadata WidgetGroupMetadata { get; }
 
+    /// <summary>
+    /// Service to provide localization functions.
+    /// </summary>
     ILocalizationService LocalizationService { get; }
 
+    /// <summary>
+    /// Service to provide log functions.
+    /// </summary>
     ILogService LogService { get; }
 
+    /// <summary>
+    /// Service to provide settings functions.
+    /// </summary>
     ISettingsService SettingsService { get; }
 
+    /// <summary>
+    /// Service to provide theme functions.
+    /// </summary>
     IThemeService ThemeService { get; }
 
+    /// <summary>
+    /// Service to provide widget functions.
+    /// </summary>
     IWidgetService WidgetService { get; }
 }
