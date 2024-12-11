e9a5bd2b0d5f93de2858d2a3f0f0b8d75de93e1c
Jack251970 <1160210343@qq.com>
2024-12-11 22:44:19 +0800

    Improve code quality.

--- DesktopWidgets3/App.xaml.cs
+++ DesktopWidgets3/App.xaml.cs
@@ -381,7 +381,7 @@ public partial class App : Application
                 Verb = admin ? "runas" : string.Empty
             });
 
-            // kill the current process
+            // Kill the current process
             Process.GetCurrentProcess().Kill();
         }
     }
